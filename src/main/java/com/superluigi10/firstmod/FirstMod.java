d3a7f6c6a6ec2dbae8f89657b0721dea4bc705a3
SuperLuigi10 <Ethan@Elizabeths-iMac.local>
2014-06-26 15:05:58 -0500

    Creation of main jar and mcmod.info

---
+++ src/main/java/com/superluigi10/firstmod/FirstMod.java
@@ -0,0 +1,33 @@
+package com.superluigi10.firstmod;
+
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="FirstMod", name="First Mod", version="1.7.2-0.1")
+public class FirstMod {
+
+    @Mod.Instance("FirstMod")
+    public static FirstMod instance;
+
+    @Mod.EventHandler
+    public void preInit (FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init (FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit (FMLPostInitializationEvent event)
+    {
+
+    }
+
+}
